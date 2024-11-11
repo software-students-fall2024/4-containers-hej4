9b3efb39ee4888ec0b19b6e5fe07d77a1f65ea11
haleyhobbs <hkh9725@nyu.edu>
2024-11-11 17:04:15 -0500

    debugging for lint

--- web-app/tests/tests_app.py
+++ web-app/tests/tests_app.py
@@ -1,4 +1,4 @@
-"""Tests for the web app."""
+"""Tests for the web app"""
 
 import sys
 from unittest.mock import patch, MagicMock
@@ -6,31 +6,30 @@ import pytest
 sys.path.append('../web-app')
 from app import create_app
 
-# test client
 @pytest.fixture
 def client():
+    """Create a test client"""
     app = create_app(test_config=True)
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-# test homepage route status and content
 def test_homepage(client):
-    """Test the homepage route for correct status and content."""
+    """Test  homepage route status and content"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Welcome to Rock-Paper-Scissors" in response.data
 
-# test play game route with valid choice
 def test_play_game(client):
+    """Test play game route for expected outcomes with valid choice"""
     response = client.post("/play", json={"choice": "rock"})
     assert response.status_code == 200
     data = response.get_json()
     assert "result" in data
     assert data["result"] in ["win", "lose", "draw"]
 
-# test storing results in database
 def test_store_game_result(client):
+    """Test storing results in database"""
     with patch("app.db.collection.insert_one") as mock_insert:
         mock_insert.return_value = MagicMock(inserted_id="fake_id")
         response = client.post("/store-result", json={"choice": "rock", "result": "win"})
