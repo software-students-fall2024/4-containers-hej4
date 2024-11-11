e23252291930c422294f93b02f03451c94cf42e4
haleyhobbs <hkh9725@nyu.edu>
2024-11-11 15:39:02 -0500

    reformatting

--- web-app/app.py
+++ web-app/app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 client = MongoClient("mongodb://mongodb:27017/")
 db = client["rockPaperScissors"]
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
