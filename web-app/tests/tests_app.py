8a48896474678811b271f35781827e849a6a1f46
ez106 <egz2006@nyu.edu>
2024-11-12 01:19:27 -0500

    dixed linting using black formatting to tests_app.py

--- web-app/tests/tests_app.py
+++ web-app/tests/tests_app.py
@@ -1,4 +1,5 @@
 """Tests for the web app"""
+
 # pylint: disable=redefined-outer-name
 import sys
 import os
@@ -46,6 +47,7 @@ def test_store_game_result(client):
         )
         assert response.status_code == 200
         mock_insert.assert_called_once_with({"choice": "rock", "result": "win"})
+
 
 def test_play_route_invalid_method(client):
     """/play should only accept POST requests, not GET"""
