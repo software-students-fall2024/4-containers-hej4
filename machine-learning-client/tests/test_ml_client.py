61db939f787570f965b5b0de4cefb263cb5a0500
haleyhobbs <hkh9725@nyu.edu>
2024-11-16 14:53:14 -0500

    pylint debug

--- machine-learning-client/tests/test_ml_client.py
+++ machine-learning-client/tests/test_ml_client.py
@@ -5,3 +5,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+# delete this when tests are added - just using for badge implementation
+def test_placeholder():
+    assert True
