cd6fe066492da19143d0dbb3ca372423d1956172
ez106 <egz2006@nyu.edu>
2024-11-18 10:18:23 -0500

    linting for tests

--- machine-learning-client/tests/test_ml_client.py
+++ machine-learning-client/tests/test_ml_client.py
@@ -4,8 +4,9 @@
 import sys
 import os
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from app import fetch_image, get_player_rps
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 # Valid base64 encoded PNG image for testing
@@ -14,7 +15,7 @@ VALID_BASE64_IMAGE = (
     "w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
 )
 
-@patch("app.db.images.find_one")  
+@patch("app.db.images.find_one")
 def test_fetch_image(mock_find_one):
     """Test fetch_image function with a mock database"""
     mock_find_one.return_value = {
@@ -27,27 +28,24 @@ def test_fetch_image(mock_find_one):
     mock_find_one.assert_called_once_with({"processed": False}, sort=[("_id", -1)])
 
 
-@patch("app.detector.findHands", return_value=(True, "modified_img"))
 @patch("app.detector.fingersUp")
-def test_rps_rock(mock_fingers_up, mock_find_hands):
+def test_rps_rock(mock_fingers_up):
     """Test get_player_rps function when gesture is rock."""
     mock_fingers_up.return_value = [0, 0, 0, 0, 0]
     result = get_player_rps(VALID_BASE64_IMAGE)
     assert result == "rock"
 
 
-@patch("app.detector.findHands", return_value=([{"id": 1}], "modified_img"))
 @patch("app.detector.fingersUp")
-def test_rps_scissors(mock_fingers_up, mock_find_hands):
+def test_rps_scissors(mock_fingers_up):
     """Test get_player_rps function when gesture is scissors."""
     mock_fingers_up.return_value = [0, 1, 1, 0, 0]
     result = get_player_rps(VALID_BASE64_IMAGE)
     assert result == "scissors"
 
 
-@patch("app.detector.findHands", return_value=(True, "modified_img"))
 @patch("app.detector.fingersUp")
-def test_rps_paper(mock_fingers_up, mock_find_hands):
+def test_rps_paper(mock_fingers_up):
     """Test get_player_rps function when gesture is paper."""
     mock_fingers_up.return_value = [1, 1, 1, 1, 1]
     result = get_player_rps(VALID_BASE64_IMAGE)
@@ -55,11 +53,10 @@ def test_rps_paper(mock_fingers_up, mock_find_hands):
 
 
 @patch("app.detector.findHands", return_value=(None, None))
-@patch("app.detector.fingersUp")
-def test_rps_no_hands(mock_fingers_up, mock_find_hands):
+def test_rps_no_hands(mock_find_hands):
     """Test get_player_rps function when there are no hands detected."""
     result = get_player_rps(VALID_BASE64_IMAGE)
-   
+    assert result is None  # Assuming None is the expected result
 
 
 if __name__ == "__main__":
